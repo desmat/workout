7f7baa10d472f6462085c168ced9445262c10dd7
Mathieu Desjarlais <github@desmat.ca>
2023-12-16 13:00:01 -0500

    colorClasses

--- app/_components/Alert.tsx
+++ app/_components/Alert.tsx
@@ -20,43 +20,67 @@ function Alert({
   handleClose: any,
 }) {
   console.log('>> app._components.Alert.Alert.render()', { message, type, closed });
-  let color;
   let icon;
+  let colorClasses;
 
   switch (type) {
     case 'error':
-      color = "red";
-      icon = <ExclamationCircleIcon className={`h-5 w-5 text-${color}-400`} aria-hidden="true" />
+      icon = <ExclamationCircleIcon className={`h-5 w-5 text-red-400`} aria-hidden="true" />
+      colorClasses = [
+        'bg-red-50',
+        'hover:bg-red-100',
+        'active:bg-red-200',
+        'text-red-800', 
+        'text-red-500',
+      ];
       break;
     case "warning":
-      color = "yellow";
-      icon = <ExclamationCircleIcon className={`h-5 w-5 text-${color}-400`} aria-hidden="true" />
+      icon = <ExclamationCircleIcon className={`h-5 w-5 text-yellow-400`} aria-hidden="true" />
+      colorClasses = [
+        'bg-yellow-50',
+        'hover:bg-yellow-100',
+        'active:bg-yellow-200',
+        'text-yellow-800', 
+        'text-yellow-500',
+      ];
       break;
     case "success":
-      color = "green";
-      icon = <CheckCircleIcon className={`h-5 w-5 text-${color}-400`} aria-hidden="true" />
+      icon = <CheckCircleIcon className={`h-5 w-5 text-green-400`} aria-hidden="true" />
+      colorClasses = [
+        'bg-green-50',
+        'hover:bg-green-100',
+        'active:bg-green-200',
+        'text-green-800', 
+        'text-green-500',
+      ];
       break;
     default: //case "info":
-      color = "blue";
-      icon = <InformationCircleIcon className={`h-5 w-5 text-${color}-400`} aria-hidden="true" />
+      icon = <InformationCircleIcon className={`h-5 w-5 text-blue-400`} aria-hidden="true" />
+      colorClasses = [
+        'bg-blue-50',
+        'hover:bg-blue-100',
+        'active:bg-blue-200',
+        'text-blue-800', 
+        'text-blue-500',
+      ];
       break;
   }
 
   return (
     <div className={`fixed bottom-3 left-3 md:left-[calc(50vw-(700px/2))] lg:left-[calc(50vw-((700px-8rem)/2))] ${closed ? "_-z-10" : "z-20"}`}>
-      <div className={`${closed ? "opacity-0" : "opacity-100"} transition-all rounded-md bg-${color}-50 p-4 w-[calc(100vw-1.5rem)] md:w-[700px] shadow-md hover:shadow-lg`}>
+      <div className={`${closed ? "opacity-0" : "opacity-100"} transition-all rounded-md ${colorClasses[0]} p-4 w-[calc(100vw-1.5rem)] md:w-[700px] shadow-md hover:shadow-lg`}>
         <div className="flex items-center">
           <div className="flex-shrink-0">
             {icon}
           </div>
           <div className="ml-3">
-            <p className={`text-sm font-medium text-${color}-800`}>{message}</p>
+            <p className={`text-sm font-medium ${colorClasses[3]}`}>{message}</p>
           </div>
           <div className="ml-auto pl-3">
             <div className="-mx-1.5 -my-1.5">
               <button
                 type="button"
-                className={`inline-flex rounded-md bg-${color}-50 p-1.5 text-${color}-500 hover:bg-${color}-100 focus:outline-none active:bg-${color}-200 _focus:ring-2 _focus:ring-${color}-600 focus:ring-offset-2 _focus:ring-offset-${color}-50`}
+                className={`inline-flex rounded-md ${colorClasses[0]} p-1.5 ${colorClasses[4]} ${colorClasses[1]} focus:outline-none ${colorClasses[2]} focus:ring-offset-2`}
                 onClick={handleClose}
               >
                 <span className="sr-only">Dismiss</span>

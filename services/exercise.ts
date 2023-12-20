da0f72a370cad5e7808130d09566dd9137729ed1
Mathieu Desjarlais <github@desmat.ca>
2023-12-20 11:05:24 -0500

    fix reading openai response

--- services/exercise.ts
+++ services/exercise.ts
@@ -20,29 +20,30 @@ async function generateExercise(name: string): Promise<any> {
   // // for testing
   // return new Promise((resolve, reject) => resolve({ prompt: "THE PROMPT", items: SAMPLE_EXERCISE.items as ExerciseItem[] }));
 
-const prompt = `Requested exercise: ${name}`;
-const completion = await openai.chat.completions.create({
-// model: 'gpt-3.5-turbo',
-model: 'gpt-4',
-// model: "gpt-3.5-turbo-1106",
-// response_format: { type: "json_object" },    
-messages: [
-{
-        role: 'system',
-content: `You are an assistant that, for the requested exercise, will generate a short description, detailed instructions, and also provide a few of variations indicating difficulty level. 
-Provide the answer in JSON using the following keys: name, description, instructions and variations. 
-The variations should have the following keys: name, level, description and instructions.`},
-{
-        role: 'user',
-content: prompt,
-}
-    ],
-});
+  const prompt = `Requested exercise: ${name}`;
+  const completion = await openai.chat.completions.create({
+  // model: 'gpt-3.5-turbo',
+  model: 'gpt-4',
+  // model: "gpt-3.5-turbo-1106",
+  // response_format: { type: "json_object" },    
+  messages: [
+  {
+          role: 'system',
+  content: `You are an assistant that, for the requested exercise, will generate a short description, detailed instructions, and also provide a few of variations indicating difficulty level. 
+  Provide the answer in JSON using the following keys: name, description, instructions and variations. 
+  The variations should have the following keys: name, level, description and instructions.`},
+  {
+          role: 'user',
+  content: prompt,
+  }
+      ],
+  });
 
   try {
-    console.log("*** RESULTS FROM API", completion);
-    console.log("*** RESULTS FROM API (as json)", JSON.stringify(JSON.parse(completion.choices[0].message.content || "{}")));
+    // console.log("*** RESULTS FROM API", completion);
+    // console.log("*** RESULTS FROM API (as json)", JSON.stringify(JSON.parse(completion.choices[0].message.content || "{}")));
     return { name, prompt, response: JSON.parse(completion.choices[0]?.message?.content || "{}") };
+    // return { name, prompt: "", response: { "name": "Push Up", "description": "A push-up is a basic bodyweight exercise that not only targets your arms, but also works your pectoral muscles, and the muscles in your shoulders, back, and abdomen to support your body while you're going through the up and down motion.", "instructions": ["1. Start in a high plank position. Plant hands directly under shoulders and keep a straight line from your head to your toes.", "2. Lower your body until your chest is an inch from the ground.", "3. Push back up to the starting position.", "4. Repeat for the desired number of repetitions."], "variations": [{ "name": "Kneeling Push Up", "level": "Beginner", "description": "Kneeling Push Up is a good starting point for beginners who find regular push-ups too challenging. It provides the similar benefits as a regular push-up but is easier to do.", "instructions": ["1. Start in a kneeling position on the floor. Place your hands shoulder-width apart, fingers facing forward.", "2. Lower your body down and push back up using your arms while keeping a straight line from your head to your knees."] }, { "name": "Clapping Push Up", "level": "Advanced", "description": "The Clapping Push Up is a more challenging variation of the classic push-up, introducing a plyometric component that forces your muscles to exert maximum force in a short period of time.", "instructions": ["1. Start in a high plank position. Plant hands directly under shoulders, while keeping a straight line from your head to your toes.", "2. Lower your body until your chest is an inch from the ground, and then push your body up forcefully to lift hand from the ground, quickly clap, and then place them back on the ground.", "3. Repeat for the desired number of repetitions."] }, { "name": "Diamond Push Up", "level": "Intermediate", "description": "The Diamond Push Up is a push-up variation that focuses more on the triceps. The diamond shape of the hands shifts the focus of the exercise.", "instructions": ["1. Start in a high plank position, but with your hands close together so your thumbs and index fingers form a diamond shape.", "2. Lower your body until your chest touches your hands.", "3. Push back up to the starting position.", "4. Repeat for the desired number of repetitions."] }] } }
   } catch (error) {
     console.error("Error reading results", { completion, error });
   }
@@ -88,19 +89,26 @@ export async function createExercise(user: User, name: string): Promise<Exercise
 
   console.log(">> services.exercise.createExercise", { res });
 
-  const parseInstruction = /(?:\d+\.?\s?)?(.*)\s?/;
+  const parseInstruction = /(?:(?:Step\s*)?\d+\.?\s*)?(.*)\s?/i;
   const fixInstructions = (exercise: any) => {
     exercise.instructions = Array.isArray(exercise.instructions)
-    ? exercise.instructions
-    : exercise.instructions
-      .split(/\n/)
-      .map((step: string) => {
-        const match = step.match(parseInstruction)
-        if (match && match.length > 0) {
-          return match[1];
-        }
-      })
-      .filter(Boolean);  
+      ? exercise.instructions
+        .map((step: string) => {
+          const match = step.match(parseInstruction)
+          if (match && match.length > 0) {
+            return match[1];
+          }
+        })
+        .filter(Boolean)
+      : exercise.instructions
+        .split(/\n/)
+        .map((step: string) => {
+          const match = step.match(parseInstruction)
+          if (match && match.length > 0) {
+            return match[1];
+          }
+        })
+        .filter(Boolean);
 
     return exercise;
   }

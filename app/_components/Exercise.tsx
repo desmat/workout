7d2070f558771a383389974abd18311c9f174865
Mathieu Desjarlais <github@desmat.ca>
2023-12-24 13:01:54 -0500

    refactor format fn's

--- app/_components/Exercise.tsx
+++ app/_components/Exercise.tsx
@@ -1,10 +1,10 @@
 import Link from "@/app/_components/Link"
-import { formatNumber, formatRange, formatTime } from "@/utils/format";
+import { formatNumber, formatTime, formatRange } from "@/utils/format";
 
 function formatDurationAndRange(duration: string | number | any[], sets: string | number | any[], reps: string | number | any[]) {
-  const formattedDuration = formatRange(duration, formatTime);
-  const formattedSets = formatRange(sets, formatNumber, formattedDuration ? " over " : "", " sets");
-  const formattedReps = formatRange(reps, formatNumber, formattedSets ? " of " : "", " reps");
+  const formattedDuration = duration && formatRange(duration, formatTime);
+  const formattedSets = sets && `${formattedDuration ? " over " : ""}${formatRange(sets, formatNumber, "set")}`
+  const formattedReps = reps && `${formattedSets ? " of " : ""}${formatRange(reps, formatNumber, "rep")}`
 
   return `${formattedDuration || ""}${formattedSets || ""}${formattedReps || ""}`
 }
@@ -14,7 +14,7 @@ export function ExerciseEntry({ exercise, user }: any) {
   const directions = formatDurationAndRange(exercise.duration, exercise.sets, exercise.reps);
   const summary = directions || exercise.description;
   console.log('>> app.exercises.page.ExerciseEntry.render()', { exercise, user, summary });
-  
+
   return (
     <Link style="parent" href={`/exercises/${exercise.id}`}>
       <span className="m-0">
--- utils/format.ts
+++ utils/format.ts
@@ -1,50 +1,46 @@
-export function formatTime(v: number | string) {
-  if (typeof (v) == "string") return [v];
+export function formatRange(range: string | number | any[], formatFn: any, unit?: string, unitMany?: string): string | undefined {
+  // for convenience
+  if (typeof (range) == "number") return formatFn(range as number, unit, unitMany);
+  if (typeof (range) == "string") return formatFn(range as string, unit, unitMany);
 
-  if (v >= 1000 * 60 * 60 * 2) return [v / 1000 / 60 / 60, "hours"];
-  if (v == 1000 * 60 * 60) return [v / 1000 / 60 / 60, "hour"];
-  if (v >= 1000 * 60 * 2) return [v / 1000 / 60, "minutes"];
-  if (v == 1000 * 60) return [v / 1000 / 60, "minute"];
-  if (v >= 1000 * 2) return [v / 1000, "seconds"];
-  if (v == 1000) return [v / 1000, "second"];
+  // when range has only one value or both range vals same, ex: [1] or [1, 1]
+  if (Array.isArray(range) && range.length < 2 || range[0] == range[1]) return formatFn(range[0], unit, unitMany);
 
-  return [v / 1000, "seconds"];
-}
+  const s0 = formatFn(range[0], unit, unitMany);
+  const s1 = formatFn(range[1], unit, unitMany);
+  const [v0, unit0] = s0.split(" ");
+  const [v1, unit1] = s1.split(" ");
+  const unitPlurialRegex = /(\w+)s/;
+  const units = [unit0, unit1].map((unit: string) => {
+    const match = unit.match(unitPlurialRegex)
+    return match && match.length > 0 && match[1] || unit;
+  });
 
-export function formatNumber(v: number | string) {
-  if (typeof (v) == "string") return [v];
-  if (typeof (v) == "number") return [`${v}`];
-
-  return [];
-}
-
-export function formatRange(range: string | number | any[], formatFn: any, prefix?: string, postfix?: string): string | undefined {
-  if (Array.isArray(range)) {
-    if (range.length > 1) {
-      if ([typeof (range[0]), typeof(range[1])].includes("number")) {
-        // maybe we'll format a range with same or different units, ex: 30-50 seconds vs 30 seconds - 2 minutes
-        const [v0, unit0] = formatFn(range[0]);
-        const [v1, unit1] = formatFn(range[1]);
-        
-        if (v0 == v1 && unit0 == unit1) return formatRange(range[0], formatFn, prefix, unit0 || postfix);
-
-        const postfix0 = unit0 && unit0 != unit1 ? ` ${unit0}` : "";
-        const postfix1 = unit1 ? ` ${unit1}` : postfix;
-        return `${formatRange(`${v0}`, formatFn, prefix, postfix0)}${formatRange(`${v1}`, formatFn, "-", postfix1)}`;
-      }
-
-      return `${formatRange(range[0], formatFn, prefix, postfix)}${formatRange(range[1], formatFn, "-", postfix)}`;
-    }
-
-    if (range.length > 0 && range[0]) {
-      return formatRange(range[0], formatFn, prefix, postfix);
-    }
+  if (units[0] == units[1]) {
+    return `${v0}-${v1} ${unit1}`;
   }
 
-  if (((typeof (range) == "string") || (typeof (range) == "number")) && range) {
-    let [v, unit] = formatFn(range);
-    return `${prefix != undefined ? prefix : ""}${v}${postfix != undefined ? postfix : unit ? ` ${unit}` : ""}`;
-  }
+  return `${s0}-${s1}`;
+}
 
-  return undefined;
+export function formatTime(v: number | string): string {
+  if (typeof (v) == "string") return v;
+
+  if (v >= 1000 * 60 * 60 * 2) return `${v / 1000 / 60 / 60} hours"`;
+  if (v == 1000 * 60 * 60) return `${v / 1000 / 60 / 60} hour`;
+  if (v >= 1000 * 60 * 2) return `${v / 1000 / 60} minutes`;
+  if (v == 1000 * 60) return `${v / 1000 / 60} minute`;
+  if (v >= 1000 * 2) return `${v / 1000} seconds`;
+  if (v == 1000) return `${v / 1000} second`;
+
+  return `${v / 1000} seconds`
+}
+
+export function formatNumber(v: number | string, unit?: string, unitMany?: string): string {
+  const n = Number(v);
+  const unitStr = n > 1
+    ? (unitMany || ` ${unit}s`)
+    : ` ${unit}`;
+
+  return `${n}${unitStr}`;
 }

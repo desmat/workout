1055b3d7cac9b58ede25087b3c93e40c5e5ef4b6
Mathieu Desjarlais <github@desmat.ca>
2024-01-01 11:58:26 -0500

    tweak front page

--- app/page.tsx
+++ app/page.tsx
@@ -21,36 +21,43 @@ export default function Component() {
 
   return (
     <Page
-      title="AI-powered app for workouts"
-      subtitle="Let AI generate for you the perfect workouts!"
+      title="AI-Powered Personal Trainer"
+      subtitle="Perfect workout plans, just for you!"
       bottomLinks={links}
     >
       <div className="self-center flex flex-col gap-1 my-2">
         <p>
-          &#8226;&nbsp;
-          <Link useClient={true} href="/exercises" style="parent" className="group">
-            Create <Link useClient={true} style="child" className="font-semibold">exercises</Link> and review AI-generated instructions and variations.
+          <Link useClient={true} href="/exercises" style="plain" className="group">
+            &#8226;&nbsp;Let AI help you with any <Link useClient={true} style="child" className="font-semibold">exercise</Link> and variation
           </Link>
         </p>
         <p>
-          &#8226;&nbsp;
-          <Link useClient={true} href="/workouts" style="plain" className="group">
-            Prepare and track your own <Link useClient={true} style="child" className="font-semibold">workouts</Link>
-          </Link>,
-          <GenerateLink style="plain" className="group">
-            &nbsp;or let our AI <Link useClient={true} style="child" className="font-semibold">generate</Link> a tailor made one for you!
+        <GenerateLink style="plain" className="group">
+          &#8226;&nbsp;<Link useClient={true} style="child" className="font-semibold">Generate</Link> your own personalized workout plans
           </GenerateLink>
         </p>
-        <p>&#8226;&nbsp;<Link useClient={true} href="/profile" style="plain" className="group">
-          <Link useClient={true} style="child" className="font-semibold">Sign-in</Link> with your email, Google or GitHub account to keep track of your progress.
-        </Link>
+        <p>
+          <Link useClient={true} href="/workouts" style="plain" className="group">
+          &#8226;&nbsp;Customize your <Link useClient={true} style="child" className="font-semibold">workouts</Link> 
+          </Link>
+        </p>
+        <p>
+        <Link useClient={true} href="/profile" style="plain" className="group">
+          &#8226;&nbsp;<Link useClient={true} style="child" className="font-semibold">Signup</Link> and track your progress over time
+          </Link>
+        </p>
+        <p>
+        &#8226;&nbsp;More to come soon!
         </p>
       </div>
       <div className="my-6 text-center">
-        <SignupOrWhatever
+        {/* <SignupOrWhatever
           signup={<Link useClient={true} href="/profile" className="font-semibold">Signup now!</Link>}
           whatever={<>More to come soon!</>}
-        />
+        /> */}
+        <Link useClient={true} href="mailto:workout@desmat.ca" style="plain" className="group">
+        Your <Link useClient={true} style="child" className="font-semibold">feedback</Link> is appreciated 🙏
+        </Link>
       </div>
     </Page>
   )
